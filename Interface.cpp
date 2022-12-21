1159566f882a2f278af5c18bc3d86952553b6b11
Goord123 <diogofgf97@gmail.com>
2022-12-21 19:29:59 +0000

    Reboot

--- Interface.cpp
+++ Interface.cpp
@@ -184,7 +184,7 @@ void Interface::start() {
                 getHelp(cmd, 3); // executa o comando help pq deu erro num comando
             }
             if(flag == -2)
-                on = false; // exit
+                on = true; // exit
             infoToUser();
         }
     } while(on);
